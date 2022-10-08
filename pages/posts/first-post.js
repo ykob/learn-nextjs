e2d0d6c5bb699e11556c560fe394b2e0529dda68
ykob <info@tplh.net>
2022-10-08 17:03:06 +0900

    added the layout component and this styles.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from "next/script";
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         <Script
@@ -19,6 +20,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
