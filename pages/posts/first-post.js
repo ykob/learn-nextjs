23c57fbb47c3359d38845e3007ee7b0d45baddeb
ykob <info@tplh.net>
2022-10-08 16:12:57 +0900

    added the Script component on first-page.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,19 @@
 import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>First Post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
       </Head>
       <h1>First Post</h1>
       <h2>
